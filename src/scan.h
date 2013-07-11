1cc464af7872d1027167d1c55ed844cdb323714e
Andrew Whitaker <whitaker@cs.washington.edu>
2013-07-10 21:48:50 -0700

    Move template inline functions inline

--- src/scan.h
+++ src/scan.h
@@ -7,37 +7,29 @@
 #include "operator.h"
 
 namespace exodb {
- 
+
 template <typename T>
 class Scan : public Operator<T> {
  public:
   explicit Scan(const std::string &file_name): file_name_(file_name),
                                                ifstream_() { }
 
-  virtual void Open();
-  virtual bool GetNext(T *out);
-  virtual void Close();
+  virtual void Open() {
+    ifstream_.open(file_name_);
+  }
+
+  virtual bool GetNext(T *out) {
+    ifstream_ >> *out;
+    return ifstream_;
+  }
+
+  virtual void Close() {
+    ifstream_.close();
+  }
 
  private:
   const std::string file_name_;
   std::ifstream ifstream_;
 };
-
-template <typename T>
-void Scan<T>::Open() {
-  ifstream_.open(file_name_);
-}
-
-template <typename T>
-void Scan<T>::Close() {
-  ifstream_.close();
-}
-
-
-template <typename T>
-bool Scan<T>::GetNext(T *out) {
-  ifstream_ >> *out;
-  return ifstream_;
-}
 
 }
